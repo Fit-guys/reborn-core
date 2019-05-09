5b4232a8c8222842cbb6777bc7a9996f486a2f08
VladGrom <gromvlad12@gmail.com>
2019-05-09 18:42:38 +0300

    add cors headers

--- src/utils/response/response.ts
+++ src/utils/response/response.ts
@@ -3,7 +3,10 @@ import { ResponseError, createError } from './response-error';
 
 export class ResponseUtils {
     public static json(res: Response, status: boolean, data?: ResponseError | any): void {
-
+        res.set("Access-Control-Allow-Credentials", "true");
+        res.set("Access-Control-Allow-Headers", "Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time");
+        res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        res.set("Access-Control-Allow-Origin", "*");
         res.json({ status, ...data });
     }
 }
