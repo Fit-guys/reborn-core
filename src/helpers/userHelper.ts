3974bcc49c7df5a54270fba1331ed5b04f681ba6
VladGrom <gromvlad12@gmail.com>
2019-05-18 22:57:48 +0300

    update rules for status

--- src/helpers/userHelper.ts
+++ src/helpers/userHelper.ts
@@ -13,15 +13,15 @@ export default class UserHelper {
     ];
     public static getUserStatus(user: User) {
         const sum = user.totalScore - user.totalTime * 0.1
-        if (sum > 5000) {
+        if (sum > 460) {
             return this.userStatuses[5];
-        } else if (sum > 4000) {
+        } else if (sum > 360) {
             return this.userStatuses[4];
-        } else if (sum > 3000) {
+        } else if (sum > 260) {
             return this.userStatuses[3];
-        } else if (sum > 2000) {
+        } else if (sum > 200) {
             return this.userStatuses[2];
-        } else if (sum > 1000) {
+        } else if (sum > 80) {
             return this.userStatuses[1];
         } else {
             return this.userStatuses[0];
